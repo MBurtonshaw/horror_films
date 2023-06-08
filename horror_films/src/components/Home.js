6ada0b784e065118cfc3f15fdcdcf1d7e3c32e58
Matt <mburtonshaw@gmail.com>
2023-06-08 18:53:11 -0400

    slideshow pics done

--- horror_films/src/components/Home.js
+++ horror_films/src/components/Home.js
@@ -56,7 +56,7 @@ export default function Home() {
             </div>
     );}
 
-    if (month === 3 || month === 4 || month === 5) {   
+    if (month === 12 || month === 1 || month === 2) {   
         {/* Dec-Feb */}
         {/* Fall/Winter Season */}
         return(
@@ -64,28 +64,34 @@ export default function Home() {
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                <img src="..." className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-                </div>
-                <div className="carousel-item">
-                <img src="..." className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-                </div>
-                <div className="carousel-item">
-                <img src="..." className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-                </div>
-                
-            </div>
+                        <a href='/'>
+                            <img className='smaller_img' src={'../../photos/hitchcock.jpeg'} alt='a black and white photo of Alfred Hitchcock'/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Alfred Hitchcock</h5>
+                                <p>Check out some of Alfred Hitchcock's classic works</p>
+                            </div>
+                        </a>
+                        </div>
+                        <div className="carousel-item">
+                        <a href='/'>
+                            <img className='smaller_img' src={'../../photos/winter.jpg'} alt='a large building and truck covered in snow'/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Winter</h5>
+                                <p>What lurks in the cold?</p>
+                            </div>
+                        </a>
+                        </div>
+                        <div className="carousel-item">
+                            <a href='/'>
+                                <img className='smaller_img' src={'../../photos/foreign.png'} alt='a girl hidden by her hair is crawling out of a tv'/>
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>Foreign Films</h5>
+                                    <p>Horror from other cultures</p>
+                                </div>
+                            </a>
+                        </div>
+                        
+                    </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
@@ -99,34 +105,40 @@ export default function Home() {
    
     );}
 
-    if (month === 12 || month === 1 || month === 2) {  
+    if (month === 3 || month === 4 || month === 5 ) {  
         {/* Mar-May */}
         {/* Spring/Nature Season */}
         return(
             <div id='Home' className='container'>
                 <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                    <img src={'../../photos/john_carpenter.jpg'} />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
+                <div className="carousel-item active">
+                        <a href='/'>
+                            <img className='smaller_img' src={'../../photos/wes_craven.jpg'} alt='a closeup photo of Wes Craven'/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Wes Craven</h5>
+                                <p>Check out some of Wes Craven's classic works</p>
+                            </div>
+                        </a>
+                        </div>
+                        <div className="carousel-item">
+                        <a href='/'>
+                            <img className='smaller_img' src={'../../photos/forest.jpeg'} alt='a gloomy looking forest'/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Scary Nature</h5>
+                                <p>Let's go camping</p>
+                            </div>
+                        </a>
+                        </div>
+                        <div className="carousel-item">
+                            <a href='/'>
+                                <img className='smaller_img' src={'../../photos/alien_horror.jpg'} alt='a large eyed grey alien'/>
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>Alien Films</h5>
+                                    <p>Horror from other worlds</p>
+                                </div>
+                            </a>
+                        </div>
                     
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -141,34 +153,40 @@ export default function Home() {
             </div>
     );}
 
-    if (month === 6 || month === 7 || month === 8) {    
+    if (month === 6 || month === 7 || month === 8 ) {    
         {/* June-Aug */}
         {/* Bright/Summer Season */}
         return(
             <div id='Home' className='container'>
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
-                <div className="carousel-item active">
-                <img src="..." className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-                </div>
-                <div className="carousel-item">
-                <img src="..." className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-                </div>
-                <div className="carousel-item">
-                <img src="..." className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-                </div>
+            <div className="carousel-item active">
+                        <a href='/'>
+                            <img className='smaller_img' src={'../../photos/desert.jpg'} alt='some bare trees in a hot looking desert'/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Desert Horror</h5>
+                                <p>Sun and sand</p>
+                            </div>
+                        </a>
+                        </div>
+                        <div className="carousel-item">
+                        <a href='/'>
+                            <img className='smaller_img' src={'../../photos/ocean.jpg'} alt='a deep blue wave and open water under a grey sky'/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Aquatic Horror</h5>
+                                <p>Don't go swimming</p>
+                            </div>
+                        </a>
+                        </div>
+                        <div className="carousel-item">
+                            <a href='/'>
+                                <img className='smaller_img' src={'../../photos/monster.jpeg'} alt='a black and white image of a disturbing smiling man'/>
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>Monster Films</h5>
+                                    <p>What is it?</p>
+                                </div>
+                            </a>
+                        </div>
                 
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">

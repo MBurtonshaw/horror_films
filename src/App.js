9c7c20911cf450ba385a1bc3aefdbe1d2c1ddc64
Matt <mburtonshaw@gmail.com>
2023-12-14 21:21:44 -0500

    color scheme created;

--- src/App.js
+++ src/App.js
@@ -14,6 +14,53 @@ import movies from './movies.json';
 
 function App() {
 
+/*
+Greens:
+rgb(50, 123, 66)
+rgb(189, 252, 203)
+rgb(137, 207, 152)
+rgb(80, 156, 96)
+rgb(32, 102, 47)
+rgb(12, 77, 26)
+rgb(2, 56, 14)
+
+Reds:
+rgb(163, 75, 66)
+rgb(250, 197, 197)
+rgb(217, 135, 126)
+rgb(194, 105, 95)
+rgb(120, 39, 30)
+rgb(59, 13, 7)
+rgb(31, 3, 3)
+
+Blues:
+rgb(55, 60, 112)
+rgb(214, 217, 252)
+rgb(140, 145, 191)
+rgb(87, 92, 145)
+rgb(33, 38, 92)
+rgb(19, 24, 74)
+rgb(1, 6, 56)
+
+Yellows:
+rgb(163, 136, 66)
+rgb(252, 226, 157)
+rgb(212, 185, 117)
+rgb(186, 160, 94)
+rgb(143, 114, 42)
+rgb(105, 79, 16)
+rgb(71, 51, 1)
+
+Greys:
+rgb(144, 150, 155)
+rgb(223, 232, 240)
+rgb(201, 210, 217)
+rgb(180, 188, 194)
+rgb(107, 111, 115)
+rgb(69, 72, 74)
+rgb(38, 39, 41)
+*/
+
 // Initializing state
 let [ month, setMonth ] = useState('');
 let [ types, setTypes ] = useState('');

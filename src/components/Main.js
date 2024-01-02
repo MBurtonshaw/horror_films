1e9b4ff342fcf70f63f6b4f9e7fd6b5d147e517d
Matt <mburtonshaw@gmail.com>
2024-01-02 18:05:06 -0500

    cleaned up photo directory

--- src/components/Main.js
+++ src/components/Main.js
@@ -64,7 +64,7 @@ export default function Main( props ) {
                     <div className='py-3'>
                         <div className='container background_box p-5 mb-5'>
                             <a href={`/titles/${ first }`}>
-                                <img id={ first } className='smaller_img py-3' src={ `../../photos/${ first }.jpg` } alt={`a movie poster for ${first}`} />
+                                <img id={ first } className='smaller_img py-3' src={ `../../photos/titles/${ first }.jpg` } alt={`a movie poster for ${first}`} />
                             </a>
                             <p className='main_text py-3 m-0'>It's Christmas, and people are celebrating; but a sorority house is receiving disturbing phone calls.</p>
                         </div>
@@ -72,7 +72,7 @@ export default function Main( props ) {
                     <div className='py-3 flashcard'>
                         <div className='container background_box p-5 mb-5'>
                             <a href={`/titles/${ second }`}>
-                                <img id={ second } className='smaller_img py-3' src={ `../../photos/${ second }.jpg` } alt={`a movie poster for ${second}`} />
+                                <img id={ second } className='smaller_img py-3' src={ `../../photos/titles/${ second }.jpg` } alt={`a movie poster for ${second}`} />
                             </a>
                             <p className='main_text py-3 m-0'>Following the Mexican-American War, a military regiment takes in a stranger on a snowy night.</p>
                         </div>
@@ -80,7 +80,7 @@ export default function Main( props ) {
                     <div className='py-3 flashcard'>
                         <div className='container background_box p-5 mb-5 flashcard'>
                             <a href={`/titles/${ third }`}>
-                                <img id={ third } className='smaller_img py-3' src={ `../../photos/${ third }.jpg` } alt={`a movie poster for ${third}`} />
+                                <img id={ third } className='smaller_img py-3' src={ `../../photos/titles/${ third }.jpg` } alt={`a movie poster for ${third}`} />
                             </a>
                             <p className='main_text py-3 m-0'>A father buys his son a rare pet as a Christmas present from a mysterious shop in Chinatown.</p>
                         </div>
@@ -88,7 +88,7 @@ export default function Main( props ) {
                     <div className='py-3 flashcard'>
                         <div className='container background_box p-5 mb-5 flashcard'>
                             <a href={`/titles/${ fourth }`}>
-                                <img id={ fourth } className='smaller_img py-3' src={ `../../photos/${ fourth }.jpg` } alt={`a movie poster for ${fourth}`} />
+                                <img id={ fourth } className='smaller_img py-3' src={ `../../photos/titles/${ fourth }.jpg` } alt={`a movie poster for ${fourth}`} />
                             </a>
                             <p className='main_text py-3 m-0'>Somewhere in Antarctica, American researchers rescue a dog and take it back to camp.</p>
                         </div>
@@ -96,7 +96,7 @@ export default function Main( props ) {
                     <div className='py-3 flashcard'>
                         <div className='container background_box p-5 mb-5 flashcard'>
                             <a href={`/titles/${ fifth }`}>
-                                <img id={ fifth } className='smaller_img py-3' src={ `../../photos/${ fifth }.jpg` } alt={`a movie poster for ${fifth}`} />
+                                <img id={ fifth } className='smaller_img py-3' src={ `../../photos/titles/${ fifth }.jpg` } alt={`a movie poster for ${fifth}`} />
                             </a>
                             <p className='main_text py-3 m-0'>A local Canadian radio host finds himself reporting on strange stories during a snowstorm.</p>
                         </div>
@@ -104,7 +104,7 @@ export default function Main( props ) {
                     <div className='py-3 flashcard'>
                         <div className='container background_box p-5 mb-5 flashcard'>
                             <a href={`/titles/${ sixth }`}>
-                                <img id={ sixth } className='smaller_img py-3' src={ `../../photos/${ sixth }.jpg` } alt={`a movie poster for ${sixth}`} />
+                                <img id={ sixth } className='smaller_img py-3' src={ `../../photos/titles/${ sixth }.jpg` } alt={`a movie poster for ${sixth}`} />
                             </a>
                             <p className='main_text py-3 m-0'>A group of people record their life in an abandoned hotel while converting it into a haunted house attraction.</p>
                         </div>
@@ -120,7 +120,7 @@ export default function Main( props ) {
                     <div className='pt-4 pb-3'>
                         <div className='container row align-items-start background_box p-5 mb-5'>
                             <a href={`/titles/${ first }`} className='col'>
-                                <img id={ first } className='smaller_img py-1' src={ `../../photos/${ first }.jpg` } alt={`a movie poster for ${first}`} />
+                                <img id={ first } className='smaller_img py-1' src={ `../../photos/titles/${ first }.jpg` } alt={`a movie poster for ${first}`} />
                             </a>
                             <p className='col m-auto main_text'>It's Christmas, and people are celebrating; but a sorority house is receiving disturbing phone calls.</p>
                         </div>
@@ -129,14 +129,14 @@ export default function Main( props ) {
                         <div className='container row align-items-start background_box p-5 mb-5'>
                             <p className='col m-auto main_text'>Following the Mexican-American War, a military regiment takes in a stranger on a snowy night.</p>
                             <a href={`/titles/${ second }`} className='col'>
-                                <img id={ second } className='smaller_img py-1' src={ `../../photos/${ second }.jpg` } alt={`a movie poster for ${second}`} />
+                                <img id={ second } className='smaller_img py-1' src={ `../../photos/titles/${ second }.jpg` } alt={`a movie poster for ${second}`} />
                             </a>
                         </div>
                     </div>
                     <div className='py-3'>
                         <div className='container row align-items-start background_box p-5 mb-5 flashcard'>
                             <a href={`/titles/${ third }`} className='col'>
-                                <img id={ third } className='smaller_img py-1' src={ `../../photos/${ third }.jpg` } alt={`a movie poster for ${third}`} />
+                                <img id={ third } className='smaller_img py-1' src={ `../../photos/titles/${ third }.jpg` } alt={`a movie poster for ${third}`} />
                             </a>
                             <p className='col m-auto main_text'>A father buys his son a rare pet as a Christmas present from a mysterious shop in Chinatown.</p>
                         </div>
@@ -145,14 +145,14 @@ export default function Main( props ) {
                         <div className='container row align-items-start background_box p-5 mb-5 flashcard'>
                             <p className='col m-auto main_text'>Somewhere in Antarctica, American researchers rescue a dog and take it back to camp.</p>
                             <a href={`/titles/${ fourth }`} className='col'>
-                                <img id={ fourth } className='smaller_img py-1' src={ `../../photos/${ fourth }.jpg` } alt={`a movie poster for ${fourth}`} />
+                                <img id={ fourth } className='smaller_img py-1' src={ `../../photos/titles/${ fourth }.jpg` } alt={`a movie poster for ${fourth}`} />
                             </a>
                         </div>
                     </div>
                     <div className='py-3'>
                         <div className='container row align-items-start background_box p-5 mb-5 flashcard'>
                             <a href={`/titles/${ fifth }`} className='col'>
-                                <img id={ fifth } className='smaller_img py-1' src={ `../../photos/${ fifth }.jpg` } alt={`a movie poster for ${fifth}`} />
+                                <img id={ fifth } className='smaller_img py-1' src={ `../../photos/titles/${ fifth }.jpg` } alt={`a movie poster for ${fifth}`} />
                             </a>
                             <p className='col m-auto main_text'>A local Canadian radio host finds himself reporting on strange stories during a snowstorm.</p>
                         </div>
@@ -161,7 +161,7 @@ export default function Main( props ) {
                         <div className='container row align-items-start background_box p-5 mb-5 flashcard'>
                             <p className='col m-auto main_text'>A group of people record their life in an abandoned hotel while converting it into a haunted house attraction.</p>
                             <a href={`/titles/${ sixth }`} className='col'>
-                                <img id={ sixth } className='smaller_img py-1' src={ `../../photos/${ sixth }.jpg` } alt={`a movie poster for ${sixth}`} />
+                                <img id={ sixth } className='smaller_img py-1' src={ `../../photos/titles/${ sixth }.jpg` } alt={`a movie poster for ${sixth}`} />
                             </a>
                         </div>
                     </div>

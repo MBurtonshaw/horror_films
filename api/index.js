556f1f9adcbe3f771b6a7f977a3e41386da4a967
Matt <mburtonshaw@gmail.com>
2024-01-10 21:40:28 -0500

    added client and api folders, can run them concurrently from root

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }));
+
+const titlesRouter = require('./routes/titles.js');
+const genresRouter = require('./routes/genres.js');
+const decadesRouter = require('./routes/decades.js');
+
+app.use('/titles', titlesRouter);
+app.use('/genres', genresRouter);
+app.use('/decades', decadesRouter);
+
+app.get('/', (req, res) => {
+    res.send({"message" : "Hi!"})
+});
+
+app.listen(5000, ()=>console.log('server is running'));

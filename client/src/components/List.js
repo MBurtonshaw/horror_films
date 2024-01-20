027c45d7e2529a2e9b415b74b3659857c9242b56
Matt <mburtonshaw@gmail.com>
2024-01-20 16:10:23 -0500

    cookies working!

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -4,35 +4,66 @@ import Cookies from 'js-cookie';
 export default function List(props) {
 
     let [ filmList, setFilmList ] = useState('');
+    let finalArray = [];
 
     async function getData() {
         let filmArray = [];
+
+        //setting film list to empty if there are no cookies yet
         if (!document.cookie) {
             setFilmList('');
+
         } else {
+
+            //looping through all movies in context and checking for cookies corresponding to the movies' [i] position
+            //this will return something for every movie on the list, so there will be 'undefined' being returned a lot
             for (let i = 0; i < props.context.data.movies.movies.length; i++) {
-                return null;
-                
-                
-                
-            }
-            
+                filmArray.push(await Cookies.get(`myList-${i}`));
+
+                function remove_undefined(val) {
+                    return val !== undefined;
+                }
+
+                //removing the undefined values from the array
+                let filteredArray = filmArray.filter(remove_undefined);
+
+                //mapping through movies in context. If a movie title matches a cookie value, it's added to filmList in state
+                props.context.data.movies.movies.map((item, i) => {
+                    for (let k = 0; k < filteredArray.length; k++) {
+                        if (item.title === filteredArray[k]) {
+                            finalArray.push(item);
+                            setFilmList(props.context.actions.removeDuplicates(finalArray));
+                        }
+                    }
+                });
+            } 
         }
     }
 
-    useEffect(()=>{ getData() }, []);
+        //function to avoid an error and check if there are no films in state on pageload
+        //if there are films present, it will return them as list items
+        function mapper() {
+            if (filmList.length < 1) {
+                return null;
+            } else {
+                return(
+                    filmList.map(
+                        (item, i) => 
+                            <li key={i} className='p-2' >
+                                <a href={`/titles/${item.url}`}> { item.title } </a>
+                            </li>
+                    )
+                );
+            }
+        }
+
+    useEffect(()=>{ getData() }, [ setFilmList ]);
 
     return(
         <div id='List' className='container w-50 p-5 mt-5 background_box'>
             <h1> My List </h1>
-            
             <ul className='p-0 pt-3'>
-                { props.context.data.movies.movies.map(
-                    (item, i) => 
-                        <li key={i} className='p-2' >
-                            <a href={`/titles/${item.url}`}> { item.title } </a>
-                        </li>
-                )}
+                { mapper() }
             </ul>
         </div>
     );
